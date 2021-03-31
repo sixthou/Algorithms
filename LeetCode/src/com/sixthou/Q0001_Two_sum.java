18d4ade75e67ab794670c32289fa0493797ac735
sixthou <justyuk@naver.com>
2021-03-31 13:40:31 +0900

    add 0001, 0007

---
+++ LeetCode/src/com/sixthou/Q0001_Two_sum.java
@@ -0,0 +1,33 @@
+package com.sixthou;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q0001_Two_sum {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{2, 7, 11, 15}, 9)));
+
+    }
+
+    static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int i = 0; i< nums.length ; i++){
+                map.put(nums[i], i);
+            }
+            int[] ans = new int[2];
+            for(int i = 0; i<nums.length ; i++){
+                int com = target - nums[i];
+                if(map.containsKey(com) && map.get(com) != i){
+                    ans[0] = i;
+                    ans[1] = map.get(com);
+                }
+            }
+            return ans;
+        }
+    }
+}
