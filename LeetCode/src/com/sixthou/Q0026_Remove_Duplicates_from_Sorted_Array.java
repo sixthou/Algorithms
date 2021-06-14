9fa10852d4c65957ad8fbaf19e58ff560e3b544f
sixthou <justyuk@naver.com>
2021-06-14 14:45:37 +0900

    Revert "2020.06.11"
    This reverts commit 5a556e74

--- LeetCode/src/com/sixthou/Q0026_Remove_Duplicates_from_Sorted_Array.java
+++ LeetCode/src/com/sixthou/Q0026_Remove_Duplicates_from_Sorted_Array.java
@@ -8,24 +8,23 @@ public class Q0026_Remove_Duplicates_from_Sorted_Array {
         Solution solution = new Solution();
         solution.removeDuplicates(nums);
     }
-    static class Solution {
-        public int removeDuplicates(int[] nums) {
-            int target = 0;
-
-            for(int i = 0 ; i< nums.length; i++){
-                if(nums[target]!=nums[i]){
-                    nums[target+1] = nums[i];
-                    target++;
-                }
-            }
-
-            return target + 1;
-
-        }
-    }
 }
 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int target = 0;
 
+        for(int i = 0 ; i< nums.length; i++){
+            if(nums[target]!=nums[i]){
+                nums[target+1] = nums[i];
+                target++;
+            }
+        }
+
+        return target + 1;
+
+    }
+}
 
 
 
