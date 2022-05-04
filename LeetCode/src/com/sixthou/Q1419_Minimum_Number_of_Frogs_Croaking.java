953beae2dc93860ee5d4f8628cd2a47bd8ac7623
sixthou <justyuk@naver.com>
2022-05-04 19:01:51 +0900

    Q1419_Minimum_Number_of_Frogs_Croaking

--- LeetCode/src/com/sixthou/Q1419_Minimum_Number_of_Frogs_Croaking.java
+++ LeetCode/src/com/sixthou/Q1419_Minimum_Number_of_Frogs_Croaking.java
@@ -1,2 +1,19 @@
-package com.sixthou;public class Q1419_Minimum_Number_of_Frogs_Croaking {
+package com.sixthou;
+
+public class Q1419_Minimum_Number_of_Frogs_Croaking {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static class Solution {
+        public int minNumberOfFrogs(String croakOfFrogs) {
+            int count = -1;
+
+
+            return -1;
+        }
+    }
+
 }
