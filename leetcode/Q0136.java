92448439f54a40f78c32c43201570546f2686240
sixthou <justyuk@naver.com>
2023-06-06 18:40:46 +0900

    [ltc]-Single Number

---
+++ leetcode/Q0136.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class Q0136 {
+    /**
+     * 제공자 : leetcode
+     * 제목 : 136. Single Number
+     * URL : https://leetcode.com/problems/single-number/
+     * 난이도 : easy
+     * 유형 : 이진탐색
+     * 설명 :
+     * 예외 :
+     **/
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            if(set.contains(num)){
+                set.remove(num);
+                continue;
+            }
+            set.add(num);
+        }
+
+        return new ArrayList<>(set).get(0);
+    }
+}
